91fb46202b8796351861894d85232fa4d29c1923
johphil <johphilencarnacion@gmail.com>
2020-07-04 13:50:40 +0800

    Separated as different projects for admin, registrar, and students entities

---
+++ EnrollmentSystem/EnrollmentStudent/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace EnrollmentSystem
+{
+    class Common
+    {
+        public static string CON_ACCOUNTDB = ConfigurationManager.ConnectionStrings["AccountDB"]?.ConnectionString;
+        public static string CON_ENROLLMENTDB = ConfigurationManager.ConnectionStrings["EnrollmentDB"]?.ConnectionString;
+
+
+        public static string ERROR_FAIL_CONNECTION = "Connection to the database could not be established.";
+    }
+}
